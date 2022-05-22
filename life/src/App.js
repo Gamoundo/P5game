523e25c87d07df9d2018fa7195bbd2165a9a91ee
Akil Griffiths <akilgg@gmail.com>
2022-05-22 18:43:34 -0400

    Added a projectile and an enemy

--- life/src/App.js
+++ life/src/App.js
@@ -11,8 +11,8 @@ function App() {
     
   }
 
-const [x, setX] =useState(30)
-const [y, setY] =useState(20)
+const [xcoord, setXCoord] =useState(30)
+const [ycoord, setYCoord] =useState(20)
 
   
   // const keyPress =({key}) => {
@@ -55,33 +55,54 @@ const [y, setY] =useState(20)
 // }, [])
 
 
-
+let c1;
+let c2;
+let c3;
+let px = 250
+let ey = 60;
   
   const draw = p5 => {
     p5.background(255, 130, 20) 
-    p5.rect(x, y, 55, 55, 20)
+    c1 = p5.color('green')
+    p5.fill(c1)
+   p5.rect(xcoord, ycoord, 55, 55, 20)
+   c2 = p5.color('yellow')
+
+   p5.fill(c2)
+    p5.ellipse(250,ey, 50,40).frameRate(40)
+    
+    ey += 5
+    if (ey > 380) {
+      ey = 0
+    }
+    c3 = p5.color('sky-blue')
+    p5.fill(c3)
+  let projectile=  p5.ellipse(px,ey, 10,5)
+
+      px -=5
+
 
     if (p5.keyCode === 39) {
-      if(x < 450){
-        setX(prev => prev +=5)
+      if(xcoord < 100){
+        setXCoord(prev => prev +=5)
       }
       
     }
     if (p5.keyCode === 37) {
-      if(x > 0){
-        setX(prev => prev -=5)
+      if(xcoord > 0){
+        setXCoord(prev => prev -=5)
       }
       
     }
     if (p5.keyCode === 38) {
-      if(y > 0) {
-        setY(prev => prev -=5)
+      if(ycoord > 0) {
+        setYCoord(prev => prev -=5)
       }
       
     }
     if (p5.keyCode === 40) {
-      if(y < 360){
-        setY(prev => prev +=5)
+      if(ycoord < 360){
+        setYCoord(prev => prev +=5)
       }
       
     }
@@ -90,7 +111,7 @@ const [y, setY] =useState(20)
   
 
   return (
-    <div className="App">
+    <div >
       <h1>Life</h1>
       <Sketch setup={setup} draw={draw}/>
     </div>
--- life/src/components/Character.js
+++ life/src/components/Character.js
@@ -1,6 +1,43 @@
 import React from 'react'
 
 export default function Character() {
+    const setup = (p5, canvasParentRef) => {
+        p5.createCanvas(500, 400).parent(canvasParentRef)
+        // getFrames(p5)
+        
+        
+      }
+      const draw = p5 => {
+        p5.background(255, 130, 20) 
+       let square= p5.rect(xcoord, ycoord, 55, 55, 20)
+        
+    square.background('yellow')
+        if (p5.keyCode === 39) {
+          if(xcoord < 100){
+            setXCoord(prev => prev +=5)
+          }
+          
+        }
+        if (p5.keyCode === 37) {
+          if(xcoord > 0){
+            setXCoord(prev => prev -=5)
+          }
+          
+        }
+        if (p5.keyCode === 38) {
+          if(ycoord > 0) {
+            setYCoord(prev => prev -=5)
+          }
+          
+        }
+        if (p5.keyCode === 40) {
+          if(ycoord < 360){
+            setYCoord(prev => prev +=5)
+          }
+          
+        }
+        
+      }
   return (
     <div>Character</div>
   )
