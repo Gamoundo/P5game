8002d0d4d137c7a12d24219db26212f2be3782ed
Akil Griffiths <akilgg@gmail.com>
2022-05-21 20:40:41 -0400

    setup movement
    basic movement with boundaries setup

--- life/src/App.js
+++ life/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Sketch from 'react-p5';
+import { useEffect, useState } from 'react';
 
 function App() {
   const setup = (p5, canvasParentRef) => {
@@ -9,8 +10,82 @@ function App() {
     
     
   }
+
+const [x, setX] =useState(30)
+const [y, setY] =useState(20)
+
+  
+  // const keyPress =({key}) => {
+  //   if(key === 'RIGHT_ARROW'){
+  //     return x +=5
+  //   }
+  //   if(key === 'LEFT_ARROW'){
+  //     return x -=5
+  //   }
+  //   if(key === 'UP_ARROW'){
+  //     return y +=5
+  //   }
+  //   if(key === 'DOWN_ARROW'){
+  //     return y -=5
+  //   }
+
+  // }
+
+// useEffect(() => {
+  
+//   const keyPress =({key}) => {
+//     if(key === 'RIGHT_ARROW'){
+//       setX(prev => prev +=5)
+//     }
+//     if(key === 'LEFT_ARROW'){
+//       setX(prev => prev -=5)
+//     }
+//     if(key === 'UP_ARROW'){
+//       setY(prev => prev +=5)
+//     }
+//     if(key === 'DOWN_ARROW'){
+//       setY(prev => prev -=5)
+//     }
+
+//   }
+
+//   window.addEventListener('keyup', keyPress)
+//   console.log(x,y)
+//   return () => window.removeEventListener('keyup', keyPress)
+// }, [])
+
+
+
+  
   const draw = p5 => {
     p5.background(255, 130, 20) 
+    p5.rect(x, y, 55, 55, 20)
+
+    if (p5.keyCode === 39) {
+      if(x < 450){
+        setX(prev => prev +=5)
+      }
+      
+    }
+    if (p5.keyCode === 37) {
+      if(x > 0){
+        setX(prev => prev -=5)
+      }
+      
+    }
+    if (p5.keyCode === 38) {
+      if(y > 0) {
+        setY(prev => prev -=5)
+      }
+      
+    }
+    if (p5.keyCode === 40) {
+      if(y < 360){
+        setY(prev => prev +=5)
+      }
+      
+    }
+    
   }
   
 
