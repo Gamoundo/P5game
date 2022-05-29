038a70514dd905a4b1c79f3d8ba4acff2c1bf1ff
Akil Griffiths <akilgg@gmail.com>
2022-05-28 23:18:30 -0400

    player aging
    player will move slower based on age, for now it is also color coded.

--- life/src/App.js
+++ life/src/App.js
@@ -12,44 +12,23 @@ function App() {
     
   }
 
-  let c1;
-  let c2;
-  let c3;
+  
+  
+  
   let px = 250
    
 
   
   
-  
+const [c1, setC1] = useState(`green`)  
 const [xcoord, setXCoord] =useState(30)
 const [ycoord, setYCoord] =useState(20)
 const [ey, setEy] = useState(60)
-
+const [age, setAge] = useState(20)
+const [speed, setSpeed] = useState(5)
 const [projectiles, setProjectiles] = useState([])
-// let projectiles = useMemo(() => [])
-
-// const projSpawn = (num) => {
-//   if(num % 15 === 0) {
-//     let bullet = {
-//       x: px,
-//       y: ey
-//     }
-//     projectiles.push(bullet)
-//   }
-  
-// }
 
 
-  
-  
-
-
-
-
-
-
-
-  
 useEffect(() => {
   if(ey % 80 === 0){
     let bullet = {
@@ -68,21 +47,29 @@ useEffect(() => {
 
   const draw = p5 => {
     p5.background(255, 130, 20) 
-    c1 = p5.color('green')
+    
     p5.fill(c1)
+    if (age > 40 && age < 60) {
+      setC1(prev => "blue")
+      setSpeed(prev => 4)
+    }
+    if (age > 60) {
+      setC1(prev => "gray")
+      setSpeed(prev => 2)
+    }
    p5.rect(xcoord, ycoord, 30, 30, 20)
-   c2 = p5.color('yellow')
+   
 
-   p5.fill(c2)
+   p5.fill('yellow')
     p5.ellipse(250,ey, 50,40).frameRate(40)
-    p5.fill(c1)
+    p5.fill('green')
     p5.text('Work', 233, ey)
     setEy(prev => prev += 5)
     if (ey > 380) {
       setEy(prev => prev = 0)
     }
-    c3 = p5.color('sky-blue')
-    p5.fill(c3)
+    
+    p5.fill('sky-blue')
   for (let bullet of projectiles) {
     p5.ellipse(bullet.x, bullet.y, 10, 5)
     bullet.x -= 5
@@ -91,25 +78,25 @@ useEffect(() => {
 
     if (p5.keyCode === 39) {
       if(xcoord < 100){
-        setXCoord(prev => prev +=5)
+        setXCoord(prev => prev +=speed)
       }
       
     }
     if (p5.keyCode === 37) {
       if(xcoord > 0){
-        setXCoord(prev => prev -=5)
+        setXCoord(prev => prev -=speed)
       }
       
     }
     if (p5.keyCode === 38) {
       if(ycoord > 0) {
-        setYCoord(prev => prev -=5)
+        setYCoord(prev => prev -=speed)
       }
       
     }
     if (p5.keyCode === 40) {
       if(ycoord < 360){
-        setYCoord(prev => prev +=5)
+        setYCoord(prev => prev +=speed)
       }
       
     }
@@ -120,8 +107,8 @@ useEffect(() => {
   return (
     <div >
       <h1>Life</h1>
-      <Timer />
-      {/* {projSpawn(ey)} */}
+      <Timer  setAge={setAge} age={age}/>
+      
       <Sketch setup={setup} draw={draw}/>
       
     </div>
