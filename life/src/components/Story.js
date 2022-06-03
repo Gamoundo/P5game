b86fca54d5a86f23df1e06110a133771393583ca
Akil Griffiths <akilgg@gmail.com>
2022-06-02 23:21:35 -0400

    story comp updated
    added conditional render based on props

--- life/src/components/Story.js
+++ life/src/components/Story.js
@@ -5,6 +5,8 @@ export default function Story(props) {
     <div> 
     <h1> The Story of {props.char.name} </h1>
     <p> {props.char.workHits}</p>
+    {(props.char.workHits >= 10 && props.char.workHits < 30) && <p> work was starting to bother {props.char.name}</p>}
+    <p> {props.char.stressHits}</p>
     </div>
   )
 }
